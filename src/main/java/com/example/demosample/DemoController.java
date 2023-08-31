c594a90f4bc077d819edb32ad7d280d1afd40321
jeet041 <mahorjitendra@gmail.com>
2023-08-31 22:32:16 +0530

    first commit

---
+++ src/main/java/com/example/demosample/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demosample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+    Logger logger = (Logger) LoggerFactory.getLogger(DemoController.class);
+    @GetMapping("/printmsg")
+    public void pringMsg(){
+        for (int i=0;i<5;i++){
+           logger.info("=========Hello World==========");
+        }
+
+    }
+}
