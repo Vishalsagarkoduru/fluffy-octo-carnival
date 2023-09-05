cce7438aed00ed425408d2d66b7bcdfacdf01951
Vishalsagarkoduru <135941640+Vishalsagarkoduru@users.noreply.github.com>
2023-09-05 18:38:14 +0530

    Update DemoController.java

--- src/main/java/com/example/demosample/DemoController.java
+++ src/main/java/com/example/demosample/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     Logger logger = (Logger) LoggerFactory.getLogger(DemoController.class);
     @GetMapping("/printmsg")
     public void pringMsg(){
-        for (int i=0;i<1000;i++){
+        for (int i=0;i<10000;i++){
            logger.info("=========Hello World=========");
         }
 }
